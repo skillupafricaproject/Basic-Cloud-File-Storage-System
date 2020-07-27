64b9e6e809252832de7e265d510e0244ea59aca8
kenny0619 <kehindeibukun0619@gmail.com>
2020-07-27 21:57:20 +0100

    ft(delete-file):[Finishes #6]

--- readDelete.js
+++ readDelete.js
@@ -1,7 +1,16 @@
 const fs = require("fs");
+//where "id" links to the path
 
 const readsFIle = (id) => {
   const dataBuffer = fs.readFileSync(id);
   const data = dataBuffer.toString();
   return data;
+};
+
+const deletesFile = (id) => {
+  try {
+    fs.unlinkSync(id);
+  } catch (err) {
+    console.error(err);
+  }
 };
